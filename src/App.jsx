41bbc0f55b13e06487c92d6666f4e0d1acefc1d8
skapildev <kd3383864@gmail.com>
2022-11-21 10:51:18 +0530

    off

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header/header";
 import Hero from "./components/HeroSlide/HeroSlide";
-import "./App.css";
 import Account from "./components/AccoutOpeningConsult/account";
 import Formation from "./components/CompanyFormation/formation";
-
 import Tax from "./components/tax/tax";
 import ServiceOffice from "./components/ServiceOffice/Serviceofice";
+// import Announce from "./components/Promotion/promotion";
 const App = () => {
   return (
     <>
@@ -16,6 +16,7 @@ const App = () => {
       <Account />
       <ServiceOffice />
       <Tax />
+      {/* <Announce /> */}
     </>
   );
 };
---
+++ src/components/Promotion/promotion.jsx
@@ -0,0 +1,30 @@
+// import React from "react";
+// import "./promotion.css";
+// import announce from "../../assets/images/home/announce-promo.jpg";
+
+// const Announce = () => {
+//   return (
+//     <div className="Promotion">
+//       <div className="promotion_head">
+//         <h2>Promotion</h2>{" "}
+//       </div>
+//       <div className="promotion-full">
+//         <div className="promo-image">
+//           <img src={announce} width={250} height={250} />
+//         </div>
+//         <div className="promo-title">
+//           <a href="link">Announcement of Fee Adjustment in October 2022</a>
+//         </div>
+//         <div className="promo-para">
+//           <p>
+//             Thank you for your continued support of One IBC. Your pleasure is
+//             indeed valuable to us, and we hope that we have succeeded in
+//             offering exceptional
+//           </p>
+//         </div>
+//       </div>
+//       <button className="promo-button">View all</button>
+//     </div>
+//   );
+// };
+// export default Announce;
