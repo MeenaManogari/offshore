15634396cc75eae8a434427d7bccffa709900ba2
MeenaManogari <akshayahps@gmail.com>
2022-11-22 16:45:17 +0530

    offshore

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/header";
-import Hero from "./components/HeroSlide/HeroSlide";
+
 import Account from "./components/AccoutOpeningConsult/account";
 import Formation from "./components/CompanyFormation/formation";
 import Tax from "./components/tax/tax";
 import ServiceOffice from "./components/ServiceOffice/Serviceofice";
-import Footer from "./components/Footer/Footer";
-import Appload from "./components/DownloadAbout/Loadabout";
+
 import Announce from "./components/Promotion/promotion";
 import Club from "./components/oneibc/oneibc";
 import Partnership from "./components/partnership/Partnership";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <>
-      <Header />
-      <Hero />
+      <Home />
       <Formation />
       <Account />
       <ServiceOffice />
@@ -23,8 +21,6 @@ const App = () => {
       <Announce />
       <Club />
       <Partnership />
-      <Appload />
-      <Footer />
     </>
   );
 };
